c17370d3ef53f11cc10f8e76ed16cce0a5e97cde
SabakaPimpek <kubaboczka1@gmail.com>
2021-11-25 17:10:20 +0100

    paddel collision

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ const ballSize = 20; //wielkość piłki
 let ballX = cw/2 - ballSize/2;
 let ballY = ch/2 - ballSize/2;
 
-let ballSpeedX = 1;
-let ballSpeedY = 1;
+let ballSpeedX = 3;
+let ballSpeedY = 3;
 
 const paddelHeight = 100;
 const paddelWidth = 20;
@@ -26,6 +26,7 @@ let player2Y = 200;
 const lineWidth = 6;
 const lineHeight = 16;
 
+// Rysowanie stołu
 function table()
 {
     ctx.fillStyle = "black";
@@ -38,6 +39,8 @@ function table()
     }
 }
 
+
+//Rysowanie piłki i odbijanie od ściany
 function ball()
 {
     ctx.fillStyle = "white";
@@ -46,12 +49,33 @@ function ball()
     ballX += ballSpeedX;
     ballY += ballSpeedY;
 
-    if(ballY === ch-ballSize) ballSpeedY = -1;
-    else if(ballX === cw-ballSize) ballSpeedX = -1;
-    else if(ballY === 0) ballSpeedY = 1;
-    else if(ballX === 0) ballSpeedX = 1;
+    if(ballY <= 0 || ballY >= ch - ballSize)
+    {
+        ballSpeedY = -ballSpeedY;
+        speedUp();
+    } 
+    else if(ballX <= 0 || ballX >= cw - ballSize)
+    {
+        ballSpeedX = -ballSpeedX;
+        speedUp();
+    }
+    //odbijanie piłki od paletki
+    else if(
+         ballX <= playerX + paddelWidth &&
+         ballX >= playerX - ballSize&&
+         ballY > playerY - ballSize/2 &&
+         ballY < playerY+paddelHeight + ballSize/2)
+    {
+        ballX += 2;
+        ballSpeedX = -ballSpeedX;
+        speedUp();
+    }
     
+
+
 }
+
+//Rysowanie Paletki
 
 function player()
 {
@@ -59,10 +83,42 @@ function player()
     ctx.fillRect(playerX, playerY, paddelWidth, paddelHeight);
 }
 
+//Rysowanie Paletki gracza 2
+
 function player2()
 {
     ctx.fillStyle = "yellow";
     ctx.fillRect(player2X, player2Y, paddelWidth, paddelHeight);
+}
+
+topCanvas = canvas.offsetTop;
+leftCanvas = canvas.offsetLeft;
+
+function playerPosition(e)
+{
+    playerY = e.clientY - topCanvas - paddelHeight/2;
+
+    if(playerY <= 0) playerY = 0;
+    else if (playerY >= ch-paddelHeight) playerY = ch-paddelHeight;
+   
+}
+
+//AI
+
+function aiPosition()
+{
+
+}
+
+canvas.addEventListener("mousemove", playerPosition);
+
+function speedUp()
+{
+    if(ballSpeedX<0 && ballSpeedX < 16) ballSpeedX -= .2;
+    if(ballSpeedX>0 && ballSpeedX < 16) ballSpeedX += .2;
+
+    if(ballSpeedY<0 && ballSpeedY < 16) ballSpeedY -= .1;
+    if(ballSpeedY>0 && ballSpeedY < 16) ballSpeedY += .1;
 }
 
 function game()
@@ -71,5 +127,14 @@ table();
 ball();
 player();
 player2();
+aiPosition();
 }
 setInterval(game, 1000/ 60);
+
+/*  else if(ballX <= 0 || ballX >= cw - ballSize)
+    {
+        ballSpeedX = -ballSpeedX;
+        speedUp();
+    }
+
+*/
